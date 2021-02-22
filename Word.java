71648bb115c1b8976d5d47eae082a695752fee1c
Kennethyeon1 <600020446@fjuhsd.org>
2021-02-22 18:15:44 +0000

    completed unit 7 lab 6

--- Word.java
+++ Word.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Kenneth Yeon
 
 import static java.lang.System.*;
 
@@ -10,12 +10,62 @@ public class Word
    																//why is it static?
    
    //add an instance variable
+  private String s;
+	//create 2 constructors
+	public Word()
+  {
+    s = "";
+  }
 
-	//create a constructor
-	
-	//create other methods
-	//look at the sample runner code
-	//on the lab handout
+  public Word(String str)
+  {
+    s = str;
+  }
 
-	//create a toString method
+  //setter method
+  public void setWord(String n)
+  {
+    s = n;
+  }
+  //getter method
+  public String getWord()
+  {
+    return s;
+  }
+  
+  //getNumVowels 
+  public int getNumVowels()
+  {
+    //create an integer called numVowel and set it equal to 0
+    int numVowel = 0;
+    //loop through each letter of the word
+    for (int i = 0; i < s.length(); i++)
+    {
+      //loop through each vowel
+      for (int a = 0; a < vowels.length(); a++)
+      {
+        //if character at i is equal to a vowel add one to numVowel
+        if (s.charAt(i) == vowels.charAt(a))
+        {
+          numVowel++;
+        }
+      }
+    }
+    //return numVowel at the end of the loop
+    return numVowel;
+  }
+
+  //getLength
+  public int getLength()
+  {
+    //create an integer called numLength and set it equal to the string length of the word
+    int numLength = s.length();
+    //return numLength
+    return numLength;
+  }
+  //create a toString method
+  public String toString()
+  {
+    return s;
+  }
 }
